2b1fbac3e4225f872405d07019ac6563c5c5f8f3
mina <sadeghbayan.mina@gmail.com>
2023-07-18 12:38:41 +0330

    Bids component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
+import Main from './shared/components/Main'
+import Bids from './shared/components/Bids'
 
 function App() {
   return (
     <>
      <MainNavigation />
+     <Main />
+     <Bids />
     </>
   )
 }
