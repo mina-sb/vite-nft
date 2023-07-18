d5fd461d93d2ea829b4eb947d0e3eac3c81af085
mina <sadeghbayan.mina@gmail.com>
2023-07-18 08:48:49 +0330

    Minor fixes in button component

--- src/shared/components/FormElements/Button.jsx
+++ src/shared/components/FormElements/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Button = (props) => {
-  console.log(props.class);
   return (
     <button class={`text-white md:px-4  duration-1000 ${props.class}`}>
       {props.children}
