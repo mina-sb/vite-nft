478665af4b2437bf434914f6bbccbfd3c8748745
mina <sadeghbayan.mina@gmail.com>
2023-07-18 20:01:32 +0330

    Minor fixes

--- src/shared/components/TopSeller.jsx
+++ src/shared/components/TopSeller.jsx
@@ -3,8 +3,8 @@ import Seller from '../components/Seller'
 
 const TopSeller = () => {
   return (
-    <section className=" bg-gray-100 mx-auto  px-4 md:px-12 lg:px-28">
-          <h3 className='font-bold text-4xl'>Top Seller in <span class="text-sky-500">1 day</span></h3>
+    <section class=" bg-gray-100 mx-auto  px-4 md:px-12 lg:px-28">
+          <h3 class='font-bold text-4xl'>Top Seller in <span class="text-sky-500">1 day</span></h3>
           <div class="h-4 mb-10">
           </div>
           <div class= "grid auto-cols-auto sm:grid-cols-2 lg:grid-cols-4 w-full gap-16">
