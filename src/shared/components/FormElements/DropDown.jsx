83eeda6f8f511ab17a99ce7666b760009546ac07
mina <sadeghbayan.mina@gmail.com>
2023-07-19 20:21:22 +0330

    Minor changes in DropDown

--- src/shared/components/FormElements/DropDown.jsx
+++ src/shared/components/FormElements/DropDown.jsx
@@ -7,7 +7,7 @@ function classNames(...classes) {
 
 export default function Dropdown() {
     return (
-        <Menu as="div" className="relative inline-block text-left my-2 w-full">
+        <Menu as="div" className="relative inline-block text-left my-1 w-full">
             <div>
                 <Menu.Button className="inline-flex justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none group  focus:ring-offset-gray-100 ">
                     Options
